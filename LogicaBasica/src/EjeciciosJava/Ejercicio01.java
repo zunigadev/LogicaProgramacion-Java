4871e44f12b3ce2867e840c89b756f84f7fc2cef
zunigadev <zuniganoriegadenzel@gmail.com>
2024-01-17 02:53:32 -0500

    Ejercicio 01 - Resuelto

--- LogicaBasica/src/EjeciciosJava/Ejercicio01.java
+++ LogicaBasica/src/EjeciciosJava/Ejercicio01.java
@@ -1,5 +1,15 @@
 package EjeciciosJava;
 
 public class Ejercicio01 {
-
+	 public static void main (String [ ] args) {
+		 for (int i=4; i<=4; i++) {
+			 for (int j=5; j<=10; j++) {
+				 System.out.println(i+"*"+j+"="+i*j);
+			 }
+		 }
+		 /*for (int i=5; i<=10; i++){
+			 int producto = 4 * i;
+			 System.out.println("4*"+i+"="+producto);
+		 };*/
+	    }
 }
---
+++ LogicaBasica/src/EjeciciosJava/Ejercicio02.java
@@ -0,0 +1,10 @@
+package EjeciciosJava;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
