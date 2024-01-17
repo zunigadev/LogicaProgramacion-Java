f888222a56538275f721927a1c6c7a6bef8ad214
zunigadev <zuniganoriegadenzel@gmail.com>
2024-01-17 16:14:27 -0500

    Ejercicio 02 - Resuelto

--- LogicaBasica/src/EjeciciosJava/Ejercicio02.java
+++ LogicaBasica/src/EjeciciosJava/Ejercicio02.java
@@ -3,7 +3,14 @@ package EjeciciosJava;
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int j=4;j<=10;j++) {
+		int i = 1;
+		System.out.println("Tabla del: "+j);
+		while(i<=10) {
+			System.out.println(j+"*"+i+"="+j*i);
+			i+=1;
+		}
+		}
 
 	}
 
